1988064f46dcffe9155d0dc29b7e3f856cb4f3ed
Yulu Xu <xuyulu86@126.com>
2022-10-16 17:21:44 +0800

    add depdency

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import audioplayer
+import games_services
 import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayerPlugin.register(with: registry.registrar(forPlugin: "AudioplayerPlugin"))
+  GamesServicesPlugin.register(with: registry.registrar(forPlugin: "GamesServicesPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
